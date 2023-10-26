bb844c7c44c226dc683be113fbb7dc0041a31cc8
Maik Froebe <maik.froebe@informatik.uni-halle.de>
2023-10-27 01:33:46 +0200

    mf

--- wise-2023/pooling/add-documents-to-pool.py
+++ wise-2023/pooling/add-documents-to-pool.py
@@ -10,7 +10,7 @@ for dataset in ['leipzig-topics-20231025-test', 'jena-topics-20231026-test']:
     data = json.load(open(f'{dataset}-pool.json', 'r'))
     for topic in data.keys():
         docs = [docs_store.get(i) for i in tqdm(data[topic]['pool'])]
-        data[topic]['pool_wit_docs'] = docs
+        data[topic]['pool_wit_docs'] = [{'doc_id': i.doc_id, 'text': i.default_text()} for i in docs]
     
     json.dump(data, open(f'{dataset}-pool-with-docs.json', 'w'))
 
