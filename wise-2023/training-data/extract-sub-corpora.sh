dfd60c45b20efb15d34cf7c0d072426fb7ae4880
Maik Fröbe <maik.froebe@uni-jena.de>
2023-11-05 22:18:58 +0100

    mf

--- wise-2023/training-data/extract-sub-corpora.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-
-docker run --rm -v /mnt/ceph/tira/state/ir_datasets:/root/.ir_datasets:ro -v ${PWD}:/foo -w /foo -ti --entrypoint python3 mam10eks/ows-long-eval-ir-datasets-integration:0.0.3 extract-sub-corpora.py
