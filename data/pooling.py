d4f3cd860855801148ab31533d2549a443ed15b4
Maik Fröbe <maik.froebe@uni-jena.de>
2024-11-13 11:12:58 +0100

    improve pooling script

--- data/pooling.py
+++ data/pooling.py
@@ -8,6 +8,9 @@ import json
 from tqdm import tqdm
 import ir_datasets
 import gzip
+from glob import glob
+from trectools import TrecRun, TrecPoolMaker
+from statistics import mean, median
 
 if not pt.started():
     pt.init()
@@ -27,21 +30,89 @@ def passage_ids(doc_id):
     return sorted(list(ret))
 
 def all_re_rankers():
-    from pyterrier_t5 import MonoT5ReRanker
-    import pyterrier_dr
+    try:
+        from pyterrier_t5 import MonoT5ReRanker
+        import pyterrier_dr
+    except: pass
+
     return [
         ('mono-t5', lambda: MonoT5ReRanker()),
-        ('colbert', lambda: pyterrier_dr.TctColBert(verbose=True))
+        ('colbert', lambda: pyterrier_dr.TctColBert(verbose=True)),
         ('ance', lambda: pyterrier_dr.Ance(verbose=True))
     ]
 
+def get_judgment_pool(directory, pooling_depth, all_docs):
+    output_file = f'{directory}/judgment-pool.json'
+    if not os.path.exists(output_file):
+        relevant_documents_per_topic = pt.io.read_topics(f'{directory}/topics.xml', 'trecxml', tags=['relevant_docnos'], tokenise=False)
+        runs = []
+        for run in glob(f'{directory}/pyterrier-*-run.gz') + glob(f'{directory}/neural-*-run.gz'):
+            runs += [TrecRun(run)]
+
+        pool = TrecPoolMaker().make_pool(runs, strategy="topX", topX=pooling_depth).pool
+        pool_sizes = []
+        for k in pool:
+            pool_sizes += [len(pool[k])]
+
+        print('Pool sizes', mean(pool_sizes), '(Mean)', ';', median(pool_sizes), '(Median).') 
+
+        for _, t in tqdm(list(relevant_documents_per_topic.iterrows()), 'Expansion Docs'):
+            docs_for_query = set()
+            relevant_docnos = set([i.strip() for i in t['query'].split(',')])
+            relevant_docnos = sorted([i for i in relevant_docnos if len(i) > 3])
+
+            for relevant_doc in relevant_docnos:
+                found = False
+                for doc in all_docs:
+                    if doc.startswith(relevant_doc + '#'):
+                        docs_for_query.add(doc)
+                        found = True
+                if not found:
+                    for doc in passage_ids(relevant_doc):
+                        docs_for_query.add(doc)
+            for doc in docs_for_query:
+                pool[t.qid].add(doc)
+
+            if len(docs_for_query) == 0 and len(relevant_docnos) != 0:
+                print('Missing relevant docs for topic', relevant_docnos)
+
+        json.dump({k: list(v) for k,v in pool.items()}, open(output_file, 'w'))
+
+    ret = json.load(open(output_file, 'r'))
+    pool_sizes = []
+    for k in ret:
+        pool_sizes += [len(ret[k])]
+    print('Pool sizes', mean(pool_sizes), '(Mean)', ';', median(pool_sizes), '(Median).')
+    return ret
+
+
+def get_index(directory):
+    if not os.path.exists(f'{directory}/pyterrier-index'):
+        if not os.path.exists(f'{directory}/documents.jsonl.gz'):
+            docs_store = ir_datasets.load('msmarco-segment-v2.1').docs_store()
+            with gzip.open(f'{directory}/documents.jsonl.gz', 'wt') as f:
+                for doc in tqdm(all_docs, 'Load Docs'):
+                    doc = docs_store.get(doc)
+                    f.write(json.dumps({'docno': doc.doc_id, 'text': doc.default_text()}) + '\n')
+                    f.flush()
+        
+        documents = []
+        with gzip.open(f'{directory}/documents.jsonl.gz', 'rt') as f:
+            for l in f:
+                documents += [json.loads(l)]
+
+        indexer = pt.IterDictIndexer(os.path.abspath(f'{directory}/pyterrier-index'), meta={'docno': 100, 'text': 20480})
+        index_ref = indexer.index(tqdm(documents, 'Index'))
+
+    return pt.IndexFactory.of(os.path.abspath(f'{directory}/pyterrier-index'))
 
 @click.command('pooling')
 @click.option('--retrieval-index', default='msmarco-passage-v2.1', help='The chatnoir index for pooling.')
 @click.option('--corpus-offset', default=1500, help='The offset for the corpus.')
 @click.option('--feedback-index', default='msmarco-document-v2.1', help='The chatnoir index on which feedback-documents are labeled.')
+@click.option('--pooling-depth', default=10, help='Pooling depth.')
 @click.argument('directory')
-def main(directory, retrieval_index, feedback_index, corpus_offset):
+def main(directory, retrieval_index, feedback_index, corpus_offset, pooling_depth):
     for query_type in ['title', 'description']:
         topics = pt.io.read_topics(f'{directory}/topics.xml', 'trecxml', tags=[query_type], tokenise=False)
 
@@ -52,9 +123,6 @@ def main(directory, retrieval_index, feedback_index, corpus_offset):
             chatnoir = ChatNoirRetrieve(index=retrieval_index, retrieval_system=retrieval_model, num_results=1500, verbose=True)
             results = chatnoir(topics)
             pt.io.write_results(results, output_file)
-
-    relevant_documents_per_topic = pt.io.read_topics(f'{directory}/topics.xml', 'trecxml', tags=['relevant_docnos'], tokenise=False)
-
 
     if not os.path.exists(f'{directory}/reformulated-feedback-documents.jsonl'):
         with open(f'{directory}/reformulated-feedback-documents.jsonl', 'w') as f:
@@ -81,29 +149,12 @@ def main(directory, retrieval_index, feedback_index, corpus_offset):
             for doc in results['docno']:
                 all_docs.add(doc)
     print('Corpus-size', len(all_docs))
-    
-    if not os.path.exists(f'{directory}/pyterrier-index'):
-        if not os.path.exists(f'{directory}/documents.jsonl.gz'):
-            docs_store = ir_datasets.load('msmarco-segment-v2.1').docs_store()
-            with gzip.open(f'{directory}/documents.jsonl.gz', 'wt') as f:
-                for doc in tqdm(all_docs, 'Load Docs'):
-                    doc = docs_store.get(doc)
-                    f.write(json.dumps({'docno': doc.doc_id, 'text': doc.default_text()}) + '\n')
-                    f.flush()
-        
-        documents = []
-        with gzip.open(f'{directory}/documents.jsonl.gz', 'rt') as f:
-            for l in f:
-                documents += [json.loads(l)]
-
-        indexer = pt.IterDictIndexer(os.path.abspath(f'{directory}/pyterrier-index'), meta={'docno': 100, 'text': 20480})
-        index_ref = indexer.index(tqdm(documents, 'Index'))
 
     for retrieval_model in ['BM25', 'PL2', 'DirichletLM', 'TF_IDF', 'Hiemstra_LM']:
-        index = pt.IndexFactory.of(os.path.abspath(f'{directory}/pyterrier-index'))
         output_file = f'{directory}/pyterrier-{retrieval_model}-run.gz'
         if os.path.exists(output_file):
             continue
+        index = get_index(directory)
         retriever = pt.BatchRetrieve(index, wmodel=retrieval_model)
         topics = pt.io.read_topics(f'{directory}/topics.xml', 'trecxml', tags=['title'], tokenise=True)
         results = retriever(topics)
@@ -134,24 +185,8 @@ def main(directory, retrieval_index, feedback_index, corpus_offset):
             results = first_stage(topics)
             pt.io.write_results(results, output_file)
 
-    for _, t in tqdm(list(relevant_documents_per_topic.iterrows()), 'Expansion Docs'):
-        docs_for_query = set()
+    judgment_pool = get_judgment_pool(directory, pooling_depth, all_docs)
 
-        relevant_docnos = set([i.strip() for i in t['query'].split(',')])
-        relevant_docnos = sorted([i for i in relevant_docnos if len(i) > 3])
-
-        for relevant_doc in relevant_docnos:
-            found = False
-            for doc in all_docs:
-                if doc.startswith(relevant_doc + '#'):
-                    docs_for_query.add(doc)
-                    found = True
-            if not found:
-                for doc in passage_ids(relevant_doc):
-                    docs_for_query.add(doc)
-
-        if len(docs_for_query) == 0 and len(relevant_docnos) != 0:
-            print('Missing relevant docs for topic', relevant_docnos)
 
 if __name__ == '__main__':
     main()
